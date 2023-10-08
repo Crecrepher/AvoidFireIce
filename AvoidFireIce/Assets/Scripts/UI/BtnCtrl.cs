90791d78945fe8ac99d42b966168d4c6f1b41463
DESKTOP-SK8C12B\USER <kartanes@naver.com>
2023-10-08 19:59:52 +0900

    메인 UI 틀 작업

---
+++ AvoidFireIce/Assets/Scripts/UI/BtnCtrl.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BtnCtrl : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    // Start is called before the first frame update
+    public BtnType currentType;
+    public Transform btnScale;
+    Vector3 defaultScale;
+
+    private void Start()
+    {
+        defaultScale = btnScale.localScale;
+    }
+    public void OnBtnClick()
+    {
+        switch (currentType)
+        {
+            case BtnType.Start:
+                Debug.Log("StartTest");
+                break;
+            case BtnType.Option:
+                break;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        btnScale.localScale = defaultScale * 1.05f; 
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        btnScale.localScale = defaultScale;
+    }
+}
